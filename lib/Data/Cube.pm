6e9dadd9c6841e4a98d59c5b87d107773f5eff29
muddy dixon <muddydixn@cpan.org>
2013-09-19 05:07:16 +0000

    Import of MUDDYDIXN/Data-Cube-0.02 from CPAN.
    gitpan-cpan-distribution: Data-Cube
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MUDDYDIXN/Data-Cube-0.02.tar.gz
    gitpan-cpan-author:       MUDDYDIXN
    gitpan-cpan-maturity:     released

--- lib/Data/Cube.pm
+++ lib/Data/Cube.pm
@@ -7,7 +7,7 @@ use Data::Dumper;
 use Data::Nest;
 use Scalar::Util qw/looks_like_number/;
 
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 
 sub new {
     my $self = shift;
@@ -261,14 +261,11 @@ sub dice {
 # すべてのセルで演算を行う
 sub rollup {
     my $self = shift;
-    my @dims = @_;
+    my %opt = @_;
 
     my @Dims = @{$self->{currentdims}};
-    if(scalar @dims > 0){
-        @Dims = @dims;
-    }
 
-    my $nest = new Data::Nest();
+    my $nest = new Data::Nest(%opt);
     foreach my $dim (@Dims){
         $nest->key($dim);
     }
@@ -311,7 +308,8 @@ Data::Cube - It's new $module
 
 =head1 DESCRIPTION
 
-Data::Cube is ...
+Data::Cube is perl implementation of [DataCube](http://en.wikipedia.org/wiki/OLAP_cube).
+DataCube is concept in order to process multidimensional data array.
 
 ==head2 METHODS
 
